382f2252e993795f106eda84d0997b2daae99d39
GitGinocchio <giuliotognetto2@gmail.com>
2025-03-16 14:14:37 +0100

    relative path fixes

--- src/utils/templates.py
+++ src/utils/templates.py
@@ -1,7 +1,9 @@
 from jinja2 import Environment, FileSystemLoader
+from markupsafe import Markup
+from functools import partial
 from .config import config
 from os import makedirs
-from os.path import dirname
+from os.path import dirname, relpath, abspath, join, normpath
 
 templates_dir : str = config['templates-dir']
 build_dir : str = config['build-dir']
@@ -9,11 +11,15 @@ build_dir : str = config['build-dir']
 loader = FileSystemLoader(templates_dir)
 env = Environment(loader=loader, autoescape=True, trim_blocks=True, lstrip_blocks=True)
 
+def relative_url(filename : str, base : str):
+    if base == '':  return normpath(filename)
+    return relpath(filename,base)
+
 templates = loader.list_templates()
 
 def build_page(template_name: str, **kwargs):
     template = env.get_template(template_name)
-    page = template.render(**kwargs)
+    page = template.render(**kwargs, relative_url=partial(relative_url, base=dirname(template_name)))
 
     template_path = f"{build_dir}/{template_name}"
 
