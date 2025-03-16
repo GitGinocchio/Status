fcf7cea33767c434e39ba41616cae84416ec2895
GitGinocchio <giuliotognetto2@gmail.com>
2025-03-16 19:49:09 +0100

    tooltip changes and service url

--- src/site/public/static/js/index.js
+++ src/site/public/static/js/index.js
@@ -1,27 +1,50 @@
-// Funzione per mostrare il tooltip
+
+let realContentTimeout = null;
+
 function showPopup(event, metric) {
-    // Rimuove eventuali tooltip esistenti
-    hidePopup();
+    let tooltip = document.querySelector('.tooltip');
+    if (!tooltip) { 
+        tooltip = document.createElement('div');
+        tooltip.className = 'tooltip';
+    }
 
-    // Crea l'elemento tooltip
-    const tooltip = document.createElement('div');
-    tooltip.className = 'tooltip';
+    tooltip.classList.remove('hidden');
 
-    // Prepara il contenuto: status e timestamp;
-    tooltip.innerHTML = `
+    const skeletonContent = `
     <div>
-        <div class="tooltip-content">
+        <div class="content">
+            <strong>Status:</strong> <span class="skeleton"></span><br>
+            <strong>Code:</strong> <span class="skeleton"></span><br>
+            ${metric.latency ? `<strong>Latency:</strong> <span class="skeleton"></span><br>` : "" }
+            ${metric.avg_latency ? `<strong>Avg Latency:</strong> <span class="skeleton"></span><br>` : "" }
+            ${metric.min_latency ? `<strong>Min Latency:</strong> <span class="skeleton"></span><br>` : "" }
+            ${metric.max_latency ? `<strong>Max Latency:</strong> <span class="skeleton"></span><br>` : "" }
+        </div>
+        <div class="footer">
+            <strong><span class="skeleton"></span></strong>
+        </div>
+    </div>
+    `;
+
+    const realContent = `
+    <div>
+        <div class="content">
             <strong>Status:</strong> ${metric.status}<br>
             <strong>Code:</strong> ${metric.code}<br>
             ${metric.latency ? `<strong>Latency:</strong> ${metric.latency} seconds<br>` : "" }
+            ${metric.avg_latency ? `<strong>Avg Latency:</strong> ${metric.avg_latency} seconds<br>` : "" }
             ${metric.min_latency ? `<strong>Min Latency:</strong> ${metric.min_latency} seconds<br>` : "" }
             ${metric.max_latency ? `<strong>Max Latency:</strong> ${metric.max_latency} seconds<br>` : "" }
         </div>
-        <div class="tooltip-footer">
+        <div class="footer">
             <strong>${metric.timestamp}</strong>
         </div>
     </div>
     `;
+
+    tooltip.innerHTML = skeletonContent;
+    clearTimeout(realContentTimeout);
+    realContentTimeout = setTimeout(() => { tooltip.innerHTML = realContent; }, 300);
 
     // Posiziona il tooltip appena sotto il cursore con un offset di 10px
     tooltip.style.left = (event.clientX + 10) + 'px';
@@ -34,7 +57,5 @@ function showPopup(event, metric) {
 // Funzione per rimuovere il tooltip
 function hidePopup() {
     const tooltip = document.querySelector('.tooltip');
-    if (tooltip) {
-        tooltip.remove();
-    }
+    tooltip.classList.add('hidden');
 }
