7e73308abc99455e7b15bb803e038625037d2efe
GitGinocchio <giuliotognetto2@gmail.com>
2025-03-23 18:38:32 +0100

    Update commons.py

--- src/utils/commons.py
+++ src/utils/commons.py
@@ -3,5 +3,4 @@ import re
 
 
 def isonline(code : int | str): 
-    print(config['success-codes'], str(code), re.match(config['success-codes'], str(code)))
     return re.match(config['success-codes'], str(code)) is not None
